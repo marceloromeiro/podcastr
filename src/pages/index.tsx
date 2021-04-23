5e5835ebcd5a1b7e39985712b17a0cc79f1c391c
Jin Souza <marceloromeiro16@gmail.com>
2021-04-22 23:19:19 -0300

    segundo dia concluido

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
 import Header from '../components/Header'
 export default function Home() {
   return (
   <h1>Index</h1>
   )
+}
+export async function getStaticProps(){
+
+
+    const response = await fetch('http://localhost:3333/episodes')
+    const data =  await response.json()
+
+  return {
+    props:{
+      episodes: data,
+    }
+  }
 }
